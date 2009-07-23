e54d03cba0ea5b45cec81a43d03840985ad5201e
void <git.email.address@googlemail.com>
2009-07-23 04:31:49 +0200

    Implemented the automatic whois on player kills - no idea if it works.

--- script/bncs.py
+++ script/bncs.py
@@ -1,5 +1,26 @@
-import utility
+import nil.string, utility, craw, threading
 
 class bncs_packet_handler_class:
+	def __init__(self):
+		self.queue_lock = threading.Lock()
+		self.whois_handler_queue = []
+		
 	def process_packet(self, packet):
-		print utility.get_packet_string(packet)
+		if len(packet) >= 2 and packet[0 : 2] == '\xff\x0f':
+			account = nil.string.extract_string(packet, '(*', ')')
+			if account == None:
+				return
+			print 'Account: %s' % account
+			self.queue_lock.acquire()
+			self.whois_handler_queue[0](account)
+			self.whois_handler_queue = self.whois_handler_queue[1 : ]
+			self.queue_lock.release()
+			
+			
+	def whois(self, name, handler):
+		self.queue_lock.acquire()
+		self.whois_handler_queue.append(handler)
+		command = '/whois %s' % name;
+		packet = '\xff\x0e' + utility.pack_number(len(command) + 5, 1) + '\x00\x00'
+		craw.send_bncs_packet(packet)
+		self.queue_lock.release()
--- script/player_kill.py
+++ script/player_kill.py
@@ -1,6 +1,44 @@
-import craw, nil.time, utility
+import craw, nil.time, utility, threading
+
+class player_information:
+	def __init__(self, name):
+		self.name = name
+		player_data = utility.get_player_data_by_name(name)
+		if player_data == None:
+			self.character_class_string = '[Unknown]'
+			print 'Unable to extract player information for name %s' % name
+		else:
+			self.character_class_string = utility.get_character_string(player_data)
+		self.account = None
+		
+	def get_string(self):
+		return '%s (*%s) (%s)' % (self.name, self.account, self.character_class_string)
+		
+
+class player_kill_whois_request:
+	def __init__(self, victim_name, killer_name):
+		self.victim = player_information(victim_name)
+		self.killer = player_information(killer_name)
 
 class player_kill_handler_class:
+	def __init__(self):
+		self.bncs_handler = None
+		self.request_lock = threading.Lock()
+		
+		self.pending_requests = []
+	
+	def victim_whois_handler(self, account):
+		self.request_lock.acquire()
+		self.pending_requests[0].victim.account = account
+		self.request_lock.release()
+		
+	def killer_whois_handler(self, account):
+		self.request_lock.acquire()
+		request = self.pending_requests[0]
+		self.pending_requests = self.pending_requests[1 : ]
+		request.killer.account = account
+		print '%s %s was slain by %s' % (nil.time.timestamp(), request.victim.get_string(), request.killer.get_string())
+		self.request_lock.release()
 
 	def process_bytes(self, bytes):
 		if len(bytes) < 26:
@@ -24,3 +62,10 @@ class player_kill_handler_class:
 		
 		line = '%s %s (%s) was slain by %s (%s) ' % (nil.time.timestamp(), victim_name, victim_string, killer_name, killer_string)
 		print line
+		
+		self.request_lock.acquire()
+		self.pending_requests.append(player_kill_whois_request(killer_name, victim_name))
+		self.request_lock.release()
+		
+		self.bncs_handler.whois(victim_name, self.victim_whois_handler)
+		self.bncs_handler.whois(killer_name, self.killer_whois_handler)
