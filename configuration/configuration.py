c50f35ada1cdccc9a89399648587b0ad656e9c9d
void <git.email.address@googlemail.com>
2009-07-26 17:51:00 +0200

    Added further base configuration data

--- configuration/configuration.py
+++ configuration/configuration.py
@@ -23,11 +23,28 @@ follow_leaders = []
 #Names of characters that are meant to follow the leader
 followers = []
 
+#Chat trigger strings for the follow bot
+all_follow_command = 'follow'
+all_stop_command = 'stop'
+
+follow_command = '%s, follow'
+stop_command = '%s, stop'
+
+enter_my_tp_command = 'enter my tp'
+
 #Names of enchanters that will listen to enchant commands issued
 enchanters = []
 
+#Chat trigger for the enchant bot
+enchant_command = 'enchant'
+enchant_mercenary_command = 'enchant mercenary'
+enchant_both_command = 'enchant us'
+
 #Names of BO barbarians for the BO bot
 battle_orders_barbarians = []
+
+#Chat triggers for the BO bot
+battle_orders_command = 'bo'
 
 """
 The following variable can either be a list of players to whom the enchant and
