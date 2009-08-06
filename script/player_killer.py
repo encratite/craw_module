6e7a8f663de86e68d38d505190cca4432b384d9b
void <git.email.address@googlemail.com>
2009-08-06 21:42:35 +0200

    Bone Prison + Bone Spear TPPK forever.

--- script/player_killer.py
+++ script/player_killer.py
@@ -15,21 +15,22 @@ teleport_skill = 0x36
 bone_spear_skill = 0x54
 bone_prison_skill = 0x58
 bone_spirit_skill = 0x5d
+
+#These are self-describing 'enums'
+attack_mode_bone_prison_bone_spirit_tppk = 'Cast Bone Prison at the target, teleport right on it, cast one Bone Spirit at it and TPPK'
+attack_mode_bone_prison_bone_spear_tppk = 'Cast Bone Prison and one Bone Spear at the target and TPPK'
+attack_mode_cast_left_skill = 'Casts the left skill at the target'
 	
 class player_killer_class:
 	def __init__(self, town_portal_handler):
 		self.movement = {}
-		self.attacking = False
+		self.attack_mode = None
 		self.town_portal_handler = town_portal_handler		
 		
 		self.debugging = True
-		self.safe_mode = True
-		self.unit_teleport = True
 		
 		self.maximal_distance = 100
 		
-		self.teleport_delay = 0.1
-		self.attack_delay = 0.4
 		self.town_portal_delay = 0.25
 		self.bone_prison_delay = 0.25
 		
@@ -43,6 +44,14 @@ class player_killer_class:
 		return player.x != 0
 		
 	def process_bytes(self, bytes):
+		set_skill = packets.parse_set_skill(bytes)
+		if set_skill != None:
+			unit_type, unit_id, side, skill = set_skill
+			#if unit_id == my_player.id:
+			if side == 0x01:
+				self.left_skill = skill
+				#print 'Left skill is set to %02x' % skill
+	
 		my_player = utility.get_my_player()
 		if my_player == None:
 			return
@@ -60,35 +69,28 @@ class player_killer_class:
 			except KeyError:
 				pass
 				
-		set_skill = packets.parse_set_skill(bytes)
 		if set_skill != None:
-			unit_type, unit_id, side, skill = set_skill
-			if unit_id == my_player.id:
-				if side == 0x01:
-					self.left_skill = skill
-					print 'Left skill is set to %02x' % skill
-					
-				if self.attacking:
-					if skill == teleport_skill:
-						self.debug('Right skill has been changed, casting teleport')
-						if self.unit_teleport:
-							packets.cast_right_skill_at_target(0, self.target.id)
-						else:
-							packets.cast_right_skill_at_location(self.teleport_location[0], self.teleport_location[1])
-					elif skill == bone_prison_skill:
-						self.debug('Right skill has been changed, casting Bone Prison')
-						packets.cast_right_skill_at_target(0, self.target.id)
-						nil.thread.create_thread(self.bone_prison_teleport)
+			if self.attack_mode == attack_mode_bone_prison_bone_spirit_tppk and skill == teleport_skill:
+				self.debug('Right skill has been changed, casting teleport')
+				packets.cast_right_skill_at_target(0, self.target.id)
+			elif self.attack_mode == attack_mode_bone_prison_bone_spirit_tppk and skill == bone_prison_skill:
+				self.debug('Right skill has been changed, casting Bone Prison for the Bone Spirit attack')
+				packets.cast_right_skill_at_target(0, self.target.id)
+				nil.thread.create_thread(self.bone_prison_teleport)
+			elif self.attack_mode == attack_mode_bone_prison_bone_spear_tppk and skill == bone_prison_skill:
+				self.debug('Right skill has been changed, casting Bone Prison for the Bone Spear attack')
+				packets.cast_right_skill_at_target(0, self.target.id)
+				nil.thread.create_thread(self.bone_prison_bone_spear)
 				
 		reassignment = packets.parse_reassignment(bytes)
 		if reassignment != None:
 			unit_type, unit_id, x, y, boolean = reassignment
-			if self.attacking and my_player.id == unit_id:
+			if self.attack_mode == attack_mode_bone_prison_bone_spirit_tppk and my_player.id == unit_id:
 				self.debug('Detected reassignment, performing attack')
 				
 				if not self.is_in_range(self.target.id):
-					print 'The player is no longer in range - unable to cast attack spell'
-					self.attacking = False
+					print 'The player is no longer in range - unable to cast Bone Spirit'
+					self.attack_mode = None
 					return
 					
 				packets.cast_left_skill_at_target(0, self.target.id)
@@ -99,20 +101,38 @@ class player_killer_class:
 				
 		if not self.is_in_range(self.target.id):
 			print 'The player is no longer in range - aborting TPPK sequence'
-			self.attacking = False
+			self.attack_mode = None
 			return
 		
 		self.debug('Casting town portal')
 		self.town_portal_handler.tppk(self.target.id)
-		self.attacking = False
+		self.attack_mode = None
 				
 	def bone_prison_teleport(self):
 		time.sleep(self.bone_prison_delay)
 		packets.set_right_skill(teleport_skill)
+		
+	def bone_prison_bone_spear(self):
+		time.sleep(self.bone_prison_delay)
+		if not self.is_in_range(self.target.id):
+			print 'The player is no longer in range - unable to cast Bone Spear'
+			self.attack_mode = None
+			return
+		packets.cast_left_skill_at_target(0, self.target.id)
+		self.town_portal()
 				
 	def debug(self, message):
 		if self.debugging:
 			print '%s (%.3f)' % (message, time.time())
+			
+	def skill_check(self, skills, attack_mode):
+		for skill in skills:
+			if craw.get_skill_level(skill) == 0:
+				print 'You lack a skill to perform the following attack: %s' % attack_mode
+				return False
+		print 'Performing attack "%s"' % attack_mode
+		self.attack_mode = attack_mode
+		return True
 
 	def attack(self):
 		if utility.town_check():
@@ -150,29 +170,12 @@ class player_killer_class:
 		self.target = target
 		self.teleport_location = (teleport_x, teleport_y)
 		
-		if self.safe_mode:
-			self.attacking = True
-			
-			if craw.get_skill_level(bone_prison_skill) > 0:
-				self.debug('Setting the right skill to Bone Prison')
-				packets.set_right_skill(bone_prison_skill)
-			else:
-				self.debug('Setting the right skill to teleport')
-				packets.set_right_skill(teleport_skill)
+		bone_spirit_attack = self.left_skill == bone_spirit_skill and self.skill_check([bone_prison_skill, teleport_skill], attack_mode_bone_prison_bone_spirit_tppk)
+		bone_spear_attack = self.left_skill == bone_spear_skill and self.skill_check([bone_prison_skill], attack_mode_bone_prison_bone_spear_tppk)
+		
+		if bone_spirit_attack or bone_spear_attack:
+			self.debug('Setting the right skill to Bone Prison')
+			packets.set_right_skill(bone_prison_skill)
 		else:
-			print 'Initiating fast attack (%.3f)' % time.time()
-			nil.thread.create_thread(self.fast_attack)
-			
-	def fast_attack(self):
-		self.debug('Launching sequence')
-		packets.set_right_skill(teleport_skill)
-		time.sleep(self.teleport_delay)
-		if self.unit_teleport:
-			packets.cast_right_skill_at_target(0, self.target.id)
-		else:
-			packets.cast_right_skill_at_location(self.teleport_location[0], self.teleport_location[1])
-		time.sleep(self.attack_delay)
-		packets.cast_left_skill_at_target(0, self.target.id)
-		time.sleep(self.town_portal_delay)
-		self.town_portal_handler.tppk(self.target.id)
-		self.debug('End of sequence')
+			print 'Performing default attack "%s"' % attack_mode_cast_left_skill
+			packets.cast_left_skill_at_target(0, self.target.id)
